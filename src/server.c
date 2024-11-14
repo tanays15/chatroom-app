73c5389ea69714bb36adaf2c1c146bea47218cc8
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2024-11-14 18:57:19 -0500

    feature: implementing server that runs on user entered port and sends Hello, World! to client

--- src/server.c
+++ src/server.c
@@ -1,13 +1,78 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include <string.h>
 #include "server.h"
 
+#define BACKLOG 10
 
-int main() {
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "incorrect arguments provided\n");
+        exit(1);
+    }
+    char *port = argv[1];
     int status;
-    struct addrinfo hints;
+    int yes = 1;
+    struct addrinfo hints, *servinfo, *p; // sets up call for getaddrinfo
+    struct sockaddr_storage inc_addr;
+    socklen_t addr_size = sizeof inc_addr;
+    int sockfd, new_fd;
+    memset(&hints, 0, sizeof(hints)); // set hints to 0 before modifying fields
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    status = getaddrinfo(NULL, port, &hints, &servinfo);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    // logic for listening on socket and port
+    for (p = servinfo; p != NULL; p = p->ai_next)  {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("server : socket\n");
+            continue;
+        }
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            perror("bind");
+            continue;
+        }
+        break;
+    }
+    freeaddrinfo(servinfo);
+    if (p == NULL) {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
 
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    } else {
+        fprintf(stdout, "server: listening on port: %s\n", port);
+    }
+
+    while(1) {
+        if ((new_fd = accept(sockfd, (struct sockaddr *) &inc_addr, &addr_size)) == -1) {
+            perror("accept");
+            continue;
+        }
+        char *msg = "Hello, World!\n";
+        int len_msg = strlen(msg);
+        if (send(new_fd, msg, len_msg, 0) == -1) {
+            perror("send");
+        }
+        close(new_fd);
+    }
+    return 0;
 }
 
