72a511814e4be7ffde1403cf526f964a127aba0a
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2024-11-15 15:11:38 -0500

    feature: implementing client for server

--- Makefile
+++ Makefile
@@ -5,16 +5,19 @@ CFLAGS = -Wall -Werror
 SRC_DIR = src
 OBJ_DIR = $(SRC_DIR)
 
+all: $(OBJ_DIR)/server $(OBJ_DIR)/client
 # target to create server executable
 
 $(OBJ_DIR)/server: $(SRC_DIR)/server.o
 	$(C) $(CFLAGS) -o $(OBJ_DIR)/server $(SRC_DIR)/server.o
 
-# target to create server.o oject file
-
 $(OBJ_DIR)/server.o: $(SRC_DIR)/server.c
 	$(C) $(CFLAGS) -c $(SRC_DIR)/server.c -o $(OBJ_DIR)/server.o
 
+$(OBJ_DIR)/client: $(SRC_DIR)/client.o
+	$(C) $(CFLAGS) -o $(OBJ_DIR)/client $(SRC_DIR)/client.o
 
+$(OBJ_DIR)/client.o: $(SRC_DIR)/client.c
+	$(C) $(CFLAGS) -c $(SRC_DIR)/client.c -o $(OBJ_DIR)/client.o
 clean:
-	rm -f $(OBJ_DIR)/server $(OBJ_DIR)/server.o
+	rm -f $(OBJ_DIR)/server $(OBJ_DIR)/server.o $(OBJ_DIR)/client $(OBJ_DIR)/client.o 
--- src/client.c
+++ src/client.c
@@ -8,7 +8,60 @@
 #include <unistd.h>
 #include <string.h>
 
+#define BACKLOG 10
+#define MAX_BUFF_SIZE 32
+
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "incorrect arguments provided\n");
+        exit(1);
+    }
+    char *port = argv[1];
+    int status;
+    struct addrinfo hints, *servinfo, *p; // sets up call for getaddrinfo
+    int sockfd;
+    memset(&hints, 0, sizeof(hints)); // set hints to 0 before modifying fields
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    status = getaddrinfo(NULL, port, &hints, &servinfo);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("client: socket\n");
+            continue;
+        }
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client connect:");
+            continue;
+        }
+        break;
+    }
+    if (p == NULL) {
+        fprintf(stderr, "Could not connect to server\n");
+        close(sockfd);
+        exit(0);
+    }
+    char buffer[MAX_BUFF_SIZE];
+    int bytes_read;
+    while (1) {
+        fscanf(stdin, "%31s", buffer); // 31 to ensure that buffer is NUL termianted
+        if ((bytes_read = send(sockfd, buffer, strlen(buffer), 0)) < 0) {
+            perror("send:");
+        }
+        if (strcmp(buffer, "exit") == 0) {
+            break;
+        }
+        if ((bytes_read = recv(sockfd, buffer, MAX_BUFF_SIZE, 0)) < 0) {
+            perror("recv:");
+        }
+        fprintf(stdout, "%s\n", buffer);
+    }
+    close(sockfd);
     return 0;
 }
