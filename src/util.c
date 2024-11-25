00d1f303f4a6d1348cc338a439b46fbc2550717d
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2024-11-25 18:05:07 -0500

    feat: updating recv_alL() function to match protocol

--- src/util.c
+++ src/util.c
@@ -2,33 +2,42 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 int send_all(int sockfd, const char *resp, int resp_len) {
   int total_bytes_sent = 0;
-  int sent;
+  int bytes_sent;
   while (total_bytes_sent < resp_len) {
-    if ((sent = send(sockfd, resp + total_bytes_sent, resp_len, 0)) == -1) {
+    if ((bytes_sent = send(sockfd, resp + total_bytes_sent, resp_len, 0)) == -1) {
       perror("send");
       break;
     }
-    total_bytes_sent += sent;
-    resp_len -= sent;
+    total_bytes_sent += bytes_sent;
+    resp_len -= bytes_sent;
   }
-  return sent == -1 ? -1 : 0;
+  return bytes_sent == -1 ? -1 : 0;
 }
 
-int recv_all(int sockfd, char *req, int req_len) {
-  int total_bytes_read = 0;
-  int bytes_read;
-  while (total_bytes_read < req_len) {
-    if ((bytes_read = recv(sockfd, req + total_bytes_read, req_len, 0)) == -1) {
-      perror("recv");
-      break;
+int recv_all(int sockfd, char *req, char **buf) {
+    int total_bytes_read = 0;
+    int bytes_read;
+    uint8_t len;
+    // read first byte for length of packet
+    *buf = malloc(len);
+    if (recv(sockfd, &len, sizeof(uint8_t), 0) == -1) {
+        fprintf(stdout, "Invalid Header\n");
+        return -1;
     }
-    total_bytes_read += bytes_read;
-    req_len -= bytes_read;
-  }
-  return bytes_read == -1 ? -1 : 0;
+    while (total_bytes_read < len) {
+        if ((bytes_read = recv(sockfd, buf + total_bytes_read, len ,0)) == -1) {
+            perror("recv");
+            break;
+        }
+        total_bytes_read += bytes_read;
+        len -= bytes_read;
+    }
+    return bytes_read == -1 ? -1 : 0;
 }
 
 void close_socket(int sockfd) {
